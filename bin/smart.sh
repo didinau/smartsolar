c5ecd41d56ec2db0e6e110daba7016aa305ce18e
D. Naujoks <59979531+didinau@users.noreply.github.com>
2024-04-15 08:46:37 +0200

    Update smart.sh

--- bin/smart.sh
+++ bin/smart.sh
@@ -1,30 +1,41 @@
 #!/bin/bash
 
+# Edit vars:
+#
+# user	user name z.B. "pi"
+#
+# wdir	source path z.B. "/usr/local/energie"
+#
+#
+#
+
+user="myadmin"
+
 wdir="/usr/local/energie"
+
 wlog="$wdir/log/smart.log"
 
-cd $wdir/log
+cd "$wdir/log"
 
 if [ "$1" != "daemon" ]; then
-	su -c "$0 daemon" myadmin
+	su -c "$0 daemon" "$user"
 else
 	(
 	exec 2>&1
-	exec 1>> $wlog
+	exec 1>>"$wlog"
 	exec 0</dev/null
 
 	export PATH="/usr/local/bin:$PATH"
 
-	#PHP="php-dist"
 	PHP="php"
 
 	while true
 	do
-		if [ -s $wdir/log/smart.log ]; then
-			cp $wlog ${wlog}.old
+		if [ -s "$wlog" ]; then
+			cp "$wlog" "${wlog}.old"
 		fi
-		>$wlog
-		$PHP $wdir/bin/smart.php 1>>$wlog 2>&1
+		> "$wlog"
+		$PHP $wdir/bin/smart.php 1>>"$wlog" 2>&1
 		sleep 10
 	done
 	) &
